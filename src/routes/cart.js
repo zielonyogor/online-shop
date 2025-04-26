c1cfb21c5e92ea5d631f96da00ad74294f522b7c
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2025-04-26 23:33:18 +0200

    Add cart page

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -3,6 +3,28 @@ const { Item } = require('../models/item');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    if (!req.session.cart) {
+        req.session.cart = [];
+    }
+
+    const ids = req.session.cart.map(cartItem => cartItem.id);
+    const items = await Item.findAll({
+        where: {
+            id: ids
+        }
+    });
+
+    const itemsInCart = items.map(item => {
+        const sessionItem = req.session.cart.find(ci => ci.id == item.id);
+        return {
+            ...item.dataValues,
+            amount: sessionItem.amount
+        };
+    });
+    res.render('cart', { itemsInCart: itemsInCart, sess: req.session });
+});
+
 router.post('/add', async (req, res) => {
     const { id, amount } = req.body;
 
@@ -20,6 +42,23 @@ router.post('/add', async (req, res) => {
 
     console.log('Cart: ', req.session.cart);
     res.redirect(`/items/${id}`);  
+});
+
+router.post('/buy', async (req, res) => {
+    const cart = req.session.cart;
+
+    cart.forEach(async ({id, amount}) => {
+        const item = await Item.findByPk(id);
+
+        if (!item) {
+           res.status(404).json({ message: 'Item not found' });
+           return;
+        }
+        
+        await item.update({...item, amount: (item.amount - amount)});
+    });
+
+    res.status(200);
 });
 
 module.exports = router;
