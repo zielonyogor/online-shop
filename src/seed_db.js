9b49530d6a79829ef7a063dbfde87ec4fecc8bb6
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2025-05-06 21:03:06 +0200

    Add more items to database, add more style to cart

--- src/seed_db.js
+++ src/seed_db.js
@@ -1,21 +1,39 @@
-const { sequelize, Item } = require('./models/index');
+const { sequelize, Item } = require('./models/item');
 
 (async () => {
     await sequelize.sync({force: true});
 
-    const catKeychain = await Item.create({
+    await Item.create({
         name: 'Little kitty keychain',
         description: 'Two-sided 6cm x 8cm keychain.',
+        image: "keychain_cat.png",
         category: 'Keychains',
         price: 9.00,
         quantity: 10
     });
 
-    const catStickerPack = await Item.create({
+    await Item.create({
         name: 'Cat sticker pack',
         description: 'A pack of 15 random cat-themed stickers stickers',
+        image: "cat_stickers.png",
         category: 'Stickers',
         price: 20.00,
         quantity: 5
+    });
+
+    await Item.create({
+        name: 'Cat portrait',
+        description: 'Personalised art of your own cat. After buying please send me a reference photos on Instagram',
+        category: 'Drawings',
+        price: 70.00,
+        quantity: 4
+    });
+
+    await Item.create({
+        name: 'Cat mug',
+        description: '330 ml mug with cat features',
+        category: 'Other',
+        price: 130.00,
+        quantity: 2
     });
 })();
